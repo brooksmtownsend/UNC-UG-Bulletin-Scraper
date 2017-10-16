ed8ce557a728c4fd155032a1b38b9287856a05d2
Brooks Townsend <brooksmtownsend@gmail.com>
2017-10-15 21:31:50 -0400

    made modifications in order to parse any file, including the professional schools

--- scrapeUgradBulletin.py
+++ scrapeUgradBulletin.py
@@ -1,14 +1,15 @@
 import re
 import json
+import sys
 
-ugradBulletin = open("./ugradBull")
+file = open(sys.argv[1])
 
 departmentOdd = "[A-Z]{3}"
 department = "[A-Z]{4}"
-courseNum = "[0-9]{3}."
+courseNum = "[0-9]{2,3}"
+endOfCourse = "[L H .][\s .]"
 
-departmentAndNumPattern = re.compile(department + "\s" + courseNum + "\s")
-departmentOddAndNumPattern = re.compile(departmentOdd + "\s" + courseNum + "\s")
+departmentAndNumPattern = re.compile(department + "\s" + courseNum + endOfCourse)
 gradingStatusPattern = re.compile("Grading status:")
 genEdPattern = re.compile("Gen Ed:")
 requisitesPattern = re.compile("Requisites:")
@@ -20,7 +21,7 @@ coursesArray = []
 class Course(object):
     arrayOfAttributes = []
     department = ""
-    courseNum = 0
+    courseNum = ""
     description = ""
     requirements = ""
     genEds = ""
@@ -33,9 +34,12 @@ class Course(object):
         # self.printAttributes()
 
     def extractInformation(self):
-        departmentAndNum = self.arrayOfAttributes[0][:8]
+        departmentAndNum = self.arrayOfAttributes[0][:9]
         self.department = departmentAndNum[:4]
-        self.courseNum = int(departmentAndNum[-3:])
+        if (departmentAndNum[-1:] == "."):
+            self.courseNum = departmentAndNum[-4:][:3]
+        else:
+            self.courseNum = departmentAndNum[-4:]
 
         for attr in self.arrayOfAttributes:
             if gradingStatusPattern.match(attr):
@@ -54,9 +58,9 @@ class Course(object):
              print(attr)
 
 
-for line in ugradBulletin:
+for line in file:
     if foundCourse:
-        match = gradingStatusPattern.match(line)
+        match = gradingStatusPattern.search(line)
         if match:
             stringArray.append(match.string)
             coursesArray.append(Course(stringArray))
@@ -64,6 +68,9 @@ for line in ugradBulletin:
             foundCourse = False
         else:
             stringArray.append(line)
+            if len(stringArray) > 15:
+                foundCourse = False
+                stringArray = []
     else:
         match = departmentAndNumPattern.match(line)
         if match:
@@ -71,8 +78,8 @@ for line in ugradBulletin:
             foundCourse = True
 
 
-# Outputting to departments.json
-text_file = open("departments.json", "w")
+# Outputting to *.json
+text_file = open(sys.argv[2], "w")
 jsonCourse = []
 
 for course in coursesArray:
